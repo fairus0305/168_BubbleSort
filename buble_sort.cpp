b02bbe843024a2aff3790e5afd013f7703d3990b
fairus0305 <ruszezzy@gmail.com>
2026-03-06 15:16:00 +0700

    menambahkan input untuk panjang array

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "masukan panjang array : ";
+        cin >> n;
+
+        if(n <= 20)
+        {
+            
+        }
+    }
+}
